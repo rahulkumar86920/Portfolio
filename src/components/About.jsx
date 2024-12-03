61415d107d3e472519339fdee1642d648fce0118
Rahul86920 <rahulkumar86920@gmail.com>
2024-12-03 12:41:16 +0530

    DOne!!

---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20 font-semibold"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hi, I'm Rahul Kumar, a web developer with a strong focus on the MERN
+          stack. My IT background drives my passion for designing innovative and
+          visually captivating software solutions that make a lasting impact.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          Bachelor of Science in Information Technology, D.G. Ruparel College,
+          2026 <br /> DSA + MERN-Stack Web Development,Apna College, 2024,
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Java, JavaScript, Node.js, React.js, MongoDB, Express.js, Git/GitHub,
+          HTML, CSS, REST APIs,
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          I’m now seeking an opportunity to start my career, eager to
+          contribute, learn, and grow in a dynamic team.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          Completion of Full-Stack Web Development (DSA + MERN STACK), Apna
+          College, 2024
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative web development solutions that exceed client expectations
+          and contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons in the ever-evolving world of
+          technology.
+        </p>
+      </div>
+      <br />
+      <br />
+      <hr />
+    </div>
+  );
+}
+
+export default About;
