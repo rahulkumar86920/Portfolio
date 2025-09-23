f7ddd050a05a3bea381baae344d1a362ac58b2d6
Rahul Kumar <rahulkumar86920@gmail.com>
2025-09-23 15:20:15 +0530

    Fix component imports for Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import Portfolio from "./components/Portfolio";
 import Experiance from "./components/Experiance";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
